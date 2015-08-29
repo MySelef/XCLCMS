8a019c8178e61ed57c99b25510b15dab336bdcdc
80213876 <80213876@qq.com>
2015-08-29 23:06:46 +0800

    update dll crop img

--- XCLCMS.View/XCLCMS.View.AdminWeb/Controllers/Common/UploadController.cs
+++ XCLCMS.View/XCLCMS.View.AdminWeb/Controllers/Common/UploadController.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using XCLNetTools.Generic;
@@ -28,21 +26,27 @@ namespace XCLCMS.View.AdminWeb.Controllers.Common
         public JsonResult UploadSubmit(FormCollection fm)
         {
             #region 基本信息
-            HttpPostedFileBase file= Request.Files["FileInfo"];
+
+            HttpPostedFileBase file = Request.Files["FileInfo"];
             DateTime dtNow = DateTime.Now;
-            Image img = null;
-            string name=string.Format("{0:yyyyMMddHHmmss}_{1}",dtNow,XCLNetTools.StringHander.RandomHelper.GetRandomValue(0,Int32.MaxValue));
-            string ext=XCLNetTools.FileHandler.ComFile.GetExtName(file.FileName);
-            string newFileName =string.Format("{0}.{1}",name, ext);
+            string name = string.Format("{0:yyyyMMddHHmmss}_{1}", dtNow, XCLNetTools.StringHander.RandomHelper.GetRandomValue(0, Int32.MaxValue));
+            string ext = XCLNetTools.FileHandler.ComFile.GetExtName(file.FileName);
+            string newFileName = string.Format("{0}.{1}", name, ext);
             //附件主目录
-            string directoryPath = string.Format("{0}/{1}/{2}/", XCLCMS.Lib.SysWebSetting.Setting.SettingModel.Admin_UploaderFilePath.TrimEnd('/'),dtNow.Year,dtNow.Month);
+            string directoryPath = string.Format("{0}/{1}/{2}/", XCLCMS.Lib.SysWebSetting.Setting.SettingModel.Admin_UploaderFilePath.TrimEnd('/'), dtNow.Year, dtNow.Month);
             //附件临时目录
             string directoryTempPath = string.Format("{0}/{1}/{2}/", XCLCMS.Lib.SysWebSetting.Setting.SettingModel.Admin_UploaderTempPath.TrimEnd('/'), dtNow.Year, dtNow.Month);
             //原图保存后的物理地址
             string savedServerPath = string.Empty;
-            #endregion
+
+            //生成目录
+            XCLNetTools.FileHandler.FileDirectory.MakeDirectory(Server.MapPath(directoryPath));
+            XCLNetTools.FileHandler.FileDirectory.MakeDirectory(Server.MapPath(directoryTempPath));
+
+            #endregion 基本信息
 
             #region 生成缩略图
+
             string fileSetting = fm["FileSetting"] ?? "";
             XCLCMS.View.Model.Uploader.FileSetting settingModel = null;
             List<XCLCMS.View.Model.Uploader.FileSetting> settingList = null;
@@ -59,21 +63,25 @@ namespace XCLCMS.View.AdminWeb.Controllers.Common
                 if (settingModel.ImgCropHeight == 0 && settingModel.ImgCropWidth == 0)
                 {
                     //如果不裁剪，则直接保存原图
-                    XCLNetTools.FileHandler.FileDirectory.MakeDirectory(Server.MapPath(directoryPath));
                     savedServerPath = Server.MapPath(string.Format("{0}/{1}", directoryPath.TrimEnd('/'), newFileName));
                 }
                 else
                 {
                     //如果裁剪了，则原图保存到临时文件夹中
-                    XCLNetTools.FileHandler.FileDirectory.MakeDirectory(Server.MapPath(directoryTempPath));
                     savedServerPath = Server.MapPath(string.Format("{0}/{1}", directoryTempPath.TrimEnd('/'), newFileName));
                 }
                 file.SaveAs(savedServerPath);
 
-                img = XCLNetTools.FileHandler.ImgLib.Crop(savedServerPath, settingModel.ImgCropWidth, settingModel.ImgCropHeight, settingModel.ImgX1, settingModel.ImgY1);
-                img.Save(Server.MapPath(string.Format("{0}/{1}_{2}_{3}.{4}", directoryPath.TrimEnd('/'), name, settingModel.ImgCropWidth, settingModel.ImgCropHeight, ext)));
+                using (var img = XCLNetTools.FileHandler.ImgLib.Crop(savedServerPath, settingModel.ImgCropWidth, settingModel.ImgCropHeight, settingModel.ImgX1, settingModel.ImgY1))
+                {
+                    if (null != img)
+                    {
+                        img.Save(Server.MapPath(string.Format("{0}/{1}_{2}_{3}.{4}", directoryPath.TrimEnd('/'), name, settingModel.ImgCropWidth, settingModel.ImgCropHeight, ext)));
+                    }
+                }
             }
-            #endregion
+
+            #endregion 生成缩略图
 
             System.Threading.Thread.Sleep(2000);
 

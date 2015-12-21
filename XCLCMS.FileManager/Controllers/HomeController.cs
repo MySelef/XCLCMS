3ff0ce265102aef155657cdf2ba4f43d0c16f48b
XCL <80213876@qq.com>
2015-12-21 22:04:31 +0800

    update file

--- XCLCMS.FileManager/Controllers/HomeController.cs
+++ XCLCMS.FileManager/Controllers/HomeController.cs
@@ -13,5 +13,20 @@ namespace XCLCMS.FileManager.Controllers
         {
             return View();
         }
+
+        public JsonResult GetFileList()
+        {
+            XCLNetTools.Message.MessageModel msg = new XCLNetTools.Message.MessageModel();
+
+            string[] files=XCLNetTools.FileHandler.ComFile.GetFolderFiles("~/");
+
+
+            string[] directories= System.IO.Directory.EnumerateDirectories(Server.MapPath("~/")).ToArray();
+
+
+            msg.CustomObject = files.Concat(directories);
+
+            return Json(msg, JsonRequestBehavior.AllowGet);
+        }
     }
 }
---
+++ XCLCMS.FileManager/Resources/Js/App.js
@@ -0,0 +1,18 @@
+﻿; (function (window,$,undefined) {
+
+    var lib = {};
+
+    lib.Home = {};
+    lib.Home.Init = function () {
+        $.XGoAjax({
+            id:"getFileList",
+            ajax: { url: AppConfig.RootUrl + "Home/GetFileList" }
+        });
+    };
+
+
+
+
+    window.App = lib;
+
+})(window,jQuery);

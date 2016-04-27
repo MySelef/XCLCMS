708ed929ebd7a51b94f36dc66dc9f66296845726
XCL <80213876@qq.com>
2016-04-27 21:36:38 +0800

    add MerchantApp

---
+++ XCLCMS.View/XCLCMS.View.AdminWeb/Controllers/Merchant/MerchantAppController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace XCLCMS.View.AdminWeb.Controllers.Merchant
+{
+    public class MerchantAppController : BaseController
+    {
+        public ActionResult List()
+        {
+            return View("~/Views/Merchant/MerchantAppList.cshtml");
+        }
+
+        public ActionResult Add()
+        {
+            return View("~/Views/Merchant/MerchantAppAdd.cshtml");
+        }
+    }
+}

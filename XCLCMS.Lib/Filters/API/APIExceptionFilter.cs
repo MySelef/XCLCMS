5aee970264fcb26161f4ced76987bf310bb1cc4b
XCL <80213876@qq.com>
2016-06-08 12:02:35 +0800

    add API/APIExceptionFilter.cs

---
+++ XCLCMS.Lib/Filters/API/APIExceptionFilter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Filters;
+
+namespace XCLCMS.Lib.Filters.API
+{
+    /// <summary>
+    /// API异常处理
+    /// </summary>
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = true, Inherited = true)]
+    public class APIExceptionFilter : FilterAttribute, IExceptionFilter
+    {
+        /// <summary>
+        /// 异常处理
+        /// </summary>
+        public Task ExecuteExceptionFilterAsync(HttpActionExecutedContext actionExecutedContext, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- XCLCMS.WebAPI/Controllers/BaseAPIController.cs
+++ XCLCMS.WebAPI/Controllers/BaseAPIController.cs
@@ -4,6 +4,7 @@
     /// web api基类
     /// </summary>
     [XCLCMS.Lib.Filters.API.APIPermissionFilter(IsMustLogin = true)]
+    [XCLCMS.Lib.Filters.API.APIExceptionFilter()]
     public class BaseAPIController : XCLCMS.Lib.Base.API.AdminBaseAPIController
     {
 

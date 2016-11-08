da8cd839b40c4f568a67248f51359a56682f7107
XCL <80213876@qq.com>
2016-11-08 20:00:14 +0800

    update Config/XConfigGen.bat

--- XCLCMS.View/XCLCMS.View.AdminWeb/Config/XConfigGen.bat
+++ XCLCMS.View/XCLCMS.View.AdminWeb/Config/XConfigGen.bat
@@ -1,5 +1,5 @@
 :bat所在目录
 set XConfigGenBatPath=%~dp0
 :执行gulp命令
-gulp --gulpfile %XConfigGenPath% --xconfig  %XConfigGenBatPath%XConfigGen-Config.json --rootpath %XConfigGenBatPath%
-pause
+node %XConfigGenPath% --xconfig  %XConfigGenBatPath%XConfigGen-Config.json --rootpath %XConfigGenBatPath%
+:pause
